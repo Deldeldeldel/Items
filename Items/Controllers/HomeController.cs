5f427c2591f70f7040f62e2dc40dd965e340cae5
Katja Valanne <katja.valanne@gmail.com>
2019-02-03 14:35:12 +0200

    Poisto lisätty.

--- Items/Controllers/HomeController.cs
+++ Items/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace Items.Controllers
             return View(context.Item);
         }
 
-        public async Task<IActionResult> Hae(string searchString)
+        public async Task<IActionResult> Hae(string searchString)  // LISÄÄ ELSE, jos stringiä ei löydy
         {
             ItemdataContext context = new ItemdataContext();
             var tavara = from t in context.Item
@@ -32,6 +32,29 @@ namespace Items.Controllers
 
             return View(await tavara.ToListAsync());
         }
+
+        public async Task<IActionResult> Lisaa( string itemName, string itemLocation, string itemClass)
+        {
+            //int itemId, aikaisemmin tuli parametrina
+            ItemdataContext context = new ItemdataContext();
+            //try
+            //{
+            //    int ItemId = Convert.ToInt32(itemId);
+            //}
+            //catch (FormatException e)
+            //{
+            //    Console.WriteLine("Input string is not a sequence of digits.");
+            //}
+            Item tavara = new Item() { ItemName = itemName, ItemLocation = itemLocation , ItemClass = itemClass};
+            context.Item.Add(tavara);
+            await context.SaveChangesAsync();
+           // ViewData["ItemId"] = itemId;
+            ViewData["ItemName"] = itemName;
+
+            //return Ok(tavara);
+            return View();
+        }
+
         //public IActionResult Hae(string itemname)
         //{
         //    ViewData["Message"] = "Hakulomake";
@@ -46,12 +69,10 @@ namespace Items.Controllers
         //    //return View();
         //}
 
-        public IActionResult Lisaa()
-        {
-            ViewData["Message"] = "Lisää tietokantaan.";
 
-            return View();
-        }
+
+
+
 
         public IActionResult Poista(int? itemId)  // hakasuluisssa tieto, mistä parametrit tulevat
         {
@@ -75,8 +96,49 @@ namespace Items.Controllers
             return View("JoPoistettu");
 
         }
+      
 
-    
+        //public IActionResult Lisaa()  // parametrina oletettu tuleva muoto
+        //{
+        //    ItemdataContext context = new ItemdataContext();
+
+        //    // Create a new Order object.
+        //    Item tavara = new Item
+        //    {
+        //        ItemId = 1,
+        //        ItemName = "laukku",
+        //        ItemLocation = "komero",
+        //        ItemBox = "-",
+        //        ItemDescription = "-",
+        //        ItemOwner = "-",
+        //        ItemClass = "-"
+        //    };
+
+        //    // Add the new object to the Orders collection.
+        //    context.Item.InsertOnSubmit(tavara);
+
+        //    // Submit the change to the database.
+        //    try
+        //    {
+        //        context.SubmitChanges();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine(e);
+        //        // Make some adjustments.
+        //        // ...
+        //        // Try again.
+        //        context.SubmitChanges();
+        //    }
+
+        //    //context.Item.Add(tavara);
+        //    //context.SaveChanges();
+        //    //ViewData["Message"] = "Lisää tietokantaan.";
+        //    //return View();
+
+        //}
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Items/Models/Item.cs
+++ Items/Models/Item.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Items.Models
 {
     public partial class Item
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ItemId { get; set; }
         public string ItemName { get; set; }
         public string ItemLocation { get; set; }
